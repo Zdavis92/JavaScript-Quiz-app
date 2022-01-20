6b1605ccfc6b79b413fd671a476047da560535f7
Zach Davis <zacharydavis92@gmail.com>
2022-01-20 12:42:19 -0600

    added questions to js

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,9 @@
 var questions = Array();
 
 questions[0] = ["Commonly used data types DO NOT include:", "Booleans", "Numbers", "Strings", "Alerts"]
-
+questions[1] = ["Arrays in Javascrpit can be used to store:", "numbers", "arrays", "booleans", "all of the above"]
+questions[2] = ["The conditions of an If/Else statement is enclosed with:", "quotation marks", "parentheses", "square brackets", "curly brackets"]
+questions[3] = ["String value must be enclosed in _______ when being assigned to a variable", "commas", "curly brackets", "quotes", "parentheses"]
+questions[4] = ["A useful tool used during development and debugging for printing content to the debugger is:", "JavaScript", "terminal/bash", "for loop", "console.log"]
 console.log(questions[0][1])
+
